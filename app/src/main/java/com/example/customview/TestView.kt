df1c1f959f81dca11e7ef39599780221ea21b770
danilserbin <dserbin@smedialink.com>
2019-03-19 15:21:05 +0300

    no message

--- app/src/main/java/com/example/customview/TestView.kt
+++ app/src/main/java/com/example/customview/TestView.kt
@@ -4,15 +4,22 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.graphics.*
+import java.lang.Math.*
 
 
 class TestView : View {
 
-    lateinit var p: Paint
-    lateinit var path: Path
-    lateinit var point1: Point
-    lateinit var point21: Point
-    lateinit var point22: Point
+    private lateinit var blue: Paint
+    private lateinit var white: Paint
+    private lateinit var red: Paint
+    private lateinit var black: Paint
+
+    private lateinit var pathRect: Path
+
+    private var R0 = 0f
+    private val R1 = 200f
+    private val R2 = 50f
+    private val angle = 30.0
 
     constructor(context: Context) : super(context) {
         initial()
@@ -27,53 +34,78 @@ class TestView : View {
     }
 
     private fun initial() {
-        p = Paint(Paint.ANTI_ALIAS_FLAG)
-        p.strokeWidth = 3f
-        path = Path()
+        blue = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.BLUE
+            }
 
-        point1 = Point(200, 300)
-        point21 = Point(500, 600)
-        point22 = Point(900, 200)
+        white = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.WHITE
+            }
+
+        red = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.RED
+            }
+
+        black = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.BLACK
+            }
+
+        pathRect = Path()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        R0 = width / 2f
 
-        canvas?.drawARGB(80, 102, 204, 255);
+        canvas?.drawCircle(width / 2f, width / 2f, R0, blue)
 
-
-        // первая линия
-        p.color = Color.BLACK;
-        canvas?.drawLine(100f, 100f, 600f, 100f, p);
-
-        // точка отклонения для первой линии
-        p.style = Paint.Style.FILL;
-        p.color = Color.GREEN;
-        canvas?.drawCircle(point1.x.toFloat(), point1.y.toFloat(), 10f, p);
-
-        // квадратичная кривая
-        path.reset();
-        path.moveTo(100f, 100f);
-        path.quadTo(point1.x.toFloat(), point1.y.toFloat(), 600f, 100f);
-        p.style = Paint.Style.STROKE;
-        canvas?.drawPath(path, p);
-
-
-        // вторая линия
-        p.color = Color.BLACK;
-        canvas?.drawLine(400f, 400f, 1100f, 400f, p);
-
-        // точки отклонения для второй линии
-        p.style = Paint.Style.FILL;
-        p.color = Color.BLUE;
-        canvas?.drawCircle(point21.x.toFloat(), point21.y.toFloat(), 10f, p);
-        canvas?.drawCircle(point22.x.toFloat(), point22.y.toFloat(), 10f, p);
-
-        // кубическая кривая
-        path.reset();
-        path.moveTo(400f, 400f);
-        path.cubicTo(point21.x.toFloat(), point21.y.toFloat(), point22.x.toFloat(), point22.y.toFloat(), 1100f, 400f);
-        p.style = Paint.Style.STROKE;
-        canvas?.drawPath(path, p);
+        circle(canvas, angle)
+        smallCircle(canvas, angle)
+        //rectangle(canvas, angle)
     }
+
+    //рисуем маленькие круги
+    private fun smallCircle(canvas: Canvas?, angle: Double) {
+        val cosA = ((R0 * R0 + (R0 - R2) * (R0 - R2) - (R2 + R1) * (R2 + R1))) / (2.0 * R0 * (R0 - R2))
+        val A = acos(cosA)
+        val B = PI * angle / 180.0
+
+        val xl = R0 - (cos(B - A)) * (R0 - R2)
+        val yl = sin(B - A) * (R0 - R2)
+
+        val xr = R0 - (cos(B + A)) * (R0 - R2)
+        val yr = sin(B + A) * (R0 - R2)
+
+        canvas?.drawCircle(xl.toFloat(), yl.toFloat() + R0, R2, red)
+        canvas?.drawCircle(xr.toFloat(), yr.toFloat() + R0, R2, red)
+    }
+
+    //рисуем средний круг
+    private fun circle(canvas: Canvas?, angle: Double) {
+        val B = PI * angle / 180
+        val y = sin(B) * R0
+        val x = (1 - cos(B)) * R0
+        canvas?.drawCircle(x.toFloat(), y.toFloat() + R0, R1, white)
+    }
+
+    /*//рисуем прямоугольник
+    private fun rectangle(canvas: Canvas?, angle: Double) {
+        val x = width / 2f
+        val y = width / 2f
+
+        pathRect.moveTo(x - 100, y - 100)
+        pathRect.lineTo(x + 100, y - 100)
+        pathRect.lineTo(x + 100, y + 100)
+        pathRect.lineTo(x - 100, y + 100)
+
+        canvas?.drawPath(pathRect, black)
+    }*/
 }
