ed0d2e63d72ff191516528c697479f8f12a6aee5
danilserbin <dserbin@smedialink.com>
2019-03-21 09:15:21 +0300

    no message

--- app/src/main/java/com/example/customview/TestView.kt
+++ app/src/main/java/com/example/customview/TestView.kt
@@ -4,20 +4,25 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.graphics.*
+import android.view.ViewTreeObserver
 import java.lang.Math.*
 
-//view правильно рисуется при R1 >= 2*R2
 class TestView : View {
 
     private lateinit var blue: Paint
     private lateinit var white: Paint
+    private lateinit var red: Paint
+    private lateinit var black: Paint
 
     private lateinit var pathTrapezium: Path
 
     private var R0 = 0f //радиус большого круга
     private var R1 = 100f //радиус среднего круга
     private var R2 = 50f //радиус маленьких кругов
-    private val angle = 3 * PI / 4
+    private val angle = 2 * PI / 5
+
+    private var xDeviation = 0f //отклонение от начала по x координат
+    private var yDeviation = 0f //отклонение от начала по y координат
 
     constructor(context: Context) : super(context) {
         initial()
@@ -44,15 +49,36 @@ class TestView : View {
                 color = Color.WHITE
             }
 
+        red = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.RED
+            }
+
+        black = Paint(Paint.ANTI_ALIAS_FLAG)
+            .apply {
+                strokeWidth = 3f
+                color = Color.BLACK
+            }
+
         pathTrapezium = Path()
+
+        this.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                R0 = width / 1f
+                xDeviation = -width / 2f
+                yDeviation = -1.75f * width
+                this@TestView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+
+        })
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        R0 = width / 2f
 
         //рисуем круг
-        canvas?.drawCircle(width / 2f, width / 2f, R0, blue)
+        canvas?.drawCircle(R0 + xDeviation, R0 + yDeviation, R0, blue)
 
         backgroundItem(canvas, angle)
     }
@@ -87,18 +113,18 @@ class TestView : View {
         val ytd = R0 + sin(B + A) * (R0 + R2 * (tan(A) - 1))
 
         //помечаем точки для трапеции
-        pathTrapezium.moveTo(xtc.toFloat(), ytc.toFloat())
-        pathTrapezium.lineTo(xtd.toFloat(), ytd.toFloat())
-        pathTrapezium.lineTo(xta.toFloat(), yta.toFloat())
-        pathTrapezium.lineTo(xtb.toFloat(), ytb.toFloat())
+        pathTrapezium.moveTo(xtc.toFloat() + xDeviation, ytc.toFloat() + yDeviation)
+        pathTrapezium.lineTo(xtd.toFloat() + xDeviation, ytd.toFloat() + yDeviation)
+        pathTrapezium.lineTo(xta.toFloat() + xDeviation, yta.toFloat() + yDeviation)
+        pathTrapezium.lineTo(xtb.toFloat() + xDeviation, ytb.toFloat() + yDeviation)
 
         //рисуем трапецию
         canvas?.drawPath(pathTrapezium, white)
         //рисуем левый маленький круг
-        canvas?.drawCircle(xl.toFloat(), yl.toFloat(), R2, blue)
+        canvas?.drawCircle(xl.toFloat() + xDeviation, yl.toFloat() + yDeviation, R2, blue)
         //рисуем правый маленький круг
-        canvas?.drawCircle(xr.toFloat(), yr.toFloat(), R2, blue)
+        canvas?.drawCircle(xr.toFloat() + xDeviation, yr.toFloat() + yDeviation, R2, blue)
         //рисуем средний круг
-        canvas?.drawCircle(x.toFloat(), y.toFloat(), R1, white)
+        canvas?.drawCircle(x.toFloat() + xDeviation, y.toFloat() + yDeviation, R1, white)
     }
 }
