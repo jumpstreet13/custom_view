ae6e22a1b90a557f52d00685c2afebb789a2d482
danilserbin <dserbin@smedialink.com>
2019-04-03 09:09:43 +0300

    add animation

--- app/src/main/java/com/example/customview/CustomToolbar.kt
+++ app/src/main/java/com/example/customview/CustomToolbar.kt
@@ -10,18 +10,19 @@ import android.view.View
 import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
+import at.wirecube.additiveanimations.additive_animator.AdditiveAnimator
 import kotlinx.android.synthetic.main.custom_toolbar.view.*
-import kotlin.math.PI
-import kotlin.math.acos
 import kotlin.math.sqrt
 
 class CustomToolbar : FrameLayout {
 
     private val whitePaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val bluePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val path = Path()
-    private val pathC = Path()
+    private val pathRect = Path()
+    private val pathFigur = Path()
+    private val pathCircle = Path()
     private lateinit var quad: Quad
+    private val listView = mutableListOf<SelectView>()
 
     private var Px0 = 0f
     private var Py0 = 0f
@@ -48,49 +49,25 @@ class CustomToolbar : FrameLayout {
         setWillNotDraw(false)
         View.inflate(context, R.layout.custom_toolbar, this)
 
-        val colorView = ContextCompat.getColor(context, R.color.blue)
-
-        bluePaint.apply {
-            color = colorView
-            strokeWidth = 3f
-            style = Paint.Style.FILL
-        }
-
-        whitePaint.apply {
-            color = Color.WHITE
-            strokeWidth = 3f
-            style = Paint.Style.FILL
-        }
-
-        viewTreeObserver?.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                Px1 = width / 2f
-                Py1 = heightRect
-                Px2 = width.toFloat()
-                Py2 = heightRect
-                Py0 = heightRect
-                heightShape = ivUsers.height.toFloat() + resources.getDimension(R.dimen.margin_normal) + 20f
-                quad = Quad(Px0, Py0, Px1, Py1, Px2, Py2)
-                viewTreeObserver.removeOnGlobalLayoutListener(this)
-            }
-
-        })
+        initPaint()
+        initCoordinate()
+        initView()
 
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        path.reset()
-        path.moveTo(Px0, Py0)
-        path.quadTo(Px1, Py1, Px2, Py2)
+        pathRect.reset()
+        pathRect.moveTo(Px0, Py0)
+        pathRect.quadTo(Px1, Py1, Px2, Py2)
 
         canvas?.drawRect(0f, 0f, width.toFloat(), heightRect, bluePaint)
-        canvas?.drawPath(path, bluePaint)
+        canvas?.drawPath(pathRect, bluePaint)
 
-        val t = (ivUsers.x + ivUsers.width / 2) / width
-        val r = (ivUsers.x - 30f) / width
-        val l = (ivUsers.x + ivUsers.width + 30f) / width.toFloat()
+        val t = (ivPost.x + ivPost.width / 2f) / width
+        val r = (ivPost.x - 30f) / width
+        val l = (ivPost.x + ivPost.width + 30f) / width
         drawShape(t, r, l, canvas)
 
     }
@@ -101,8 +78,25 @@ class CustomToolbar : FrameLayout {
         invalidate()
     }
 
+    fun select(position: Int) {
+        val currentView = listView.find { it.select }
+        val selectView = listView.find { it.position == position }
+
+        if (currentView?.position == selectView?.position) return
+
+        currentView?.select = false
+        selectView?.select = true
+
+        selectView?.let { select ->
+            AdditiveAnimator.animate(select.view).apply {
+                translationY(heightRect - select.view.height)
+                start()
+            }
+        }
+    }
+
     private fun drawShape(t: Float, r: Float, l: Float, canvas: Canvas?) {
-        val k = 50f//
+        val k = 30f//
         val p = 30f//
 
         val x = quad.getX(t)
@@ -124,28 +118,70 @@ class CustomToolbar : FrameLayout {
         val xd = xa - k
         val yd = ya - k
 
-        val xo = (xa + xb) / 2
-        val yo = (yb + ya) / 2 - k
-
         val ro = sqrt((xc - xd) * (xc - xd) + (yc - yd) * (yc - yd)) / 2
 
-        val xm = (xa + xd) / 2 - p
-        val ym = (ya + yd) / 2 + p
+        val xm = (xa + xd) / 2 - k
+        val ym = (ya + yd) / 2 - k
 
-        val xn = (xb + xc) / 2 + p
-        val yn = (yb + yc) / 2 + p
+        val xn = (xb + xc) / 2 + k
+        val yn = (yb + yc) / 2 - k
 
-        pathC.reset()
-        pathC.moveTo(xa, ya)
-        pathC.quadTo(xm, ym, xd, yd)
-        pathC.lineTo(xc, yc)
-        pathC.quadTo(xn, yn, xb, yb)
+        pathFigur.reset()
+        pathFigur.moveTo(xa, ya)
+        pathFigur.quadTo(xm, ym, xd, yd)
+        pathFigur.lineTo(xb, yb)
+        pathFigur.quadTo(xn, yn, xc, yc)
 
-        //рисуем трапецию
-        canvas?.drawPath(pathC, whitePaint)
+        pathCircle.addOval(x - ro, y - ro - 10f, x + ro, y + ro - 10f, Path.Direction.CCW)
+
         //рисуем круг
-        canvas?.drawOval(xo - ro, yo + ro, xo + ro, yo - ro, whitePaint)
+        //canvas?.drawPath(pathCircle, whitePaint)
+        //рисуем трапецию
+        //canvas?.drawPath(pathFigur, whitePaint)
 
+    }
+
+    private fun initPaint() {
+
+        bluePaint.apply {
+            color = ContextCompat.getColor(context, R.color.blue)
+            strokeWidth = 3f
+            style = Paint.Style.FILL
+        }
+
+        whitePaint.apply {
+            color = Color.WHITE
+            strokeWidth = 3f
+            style = Paint.Style.FILL
+        }
+    }
+
+    private fun initCoordinate() {
+
+        viewTreeObserver?.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                Px1 = width / 2f
+                Py1 = heightRect
+                Px2 = width.toFloat()
+                Py2 = heightRect
+                Py0 = heightRect
+                heightShape = ivUsers.height.toFloat() + resources.getDimension(R.dimen.margin_normal) + 20f
+                quad = Quad(Px0, Py0, Px1, Py1, Px2, Py2)
+
+                select(0)
+
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+
+        })
+
+    }
+
+    private fun initView() {
+        listView.add(SelectView(0, ivPost, false))
+        listView.add(SelectView(1, ivUsers, false))
+        listView.add(SelectView(2, ivTags, false))
+        listView.add(SelectView(3, ivPlace, false))
     }
 
     class Quad(
@@ -159,5 +195,7 @@ class CustomToolbar : FrameLayout {
         fun getX(t: Float) = (1 - t) * (1 - t) * Px0 + 2 * t * (1 - t) * Px1 + t * t * Px2
         fun getY(t: Float) = (1 - t) * (1 - t) * Py0 + 2 * t * (1 - t) * Py1 + t * t * Py2
     }
+
+    data class SelectView(val position: Int, val view: View, var select: Boolean)
 
 }
